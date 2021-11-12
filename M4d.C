70c6a30a56ef3a32795d1967880dd42397bd9595
Paisen <90249855+MadGics@users.noreply.github.com>
2021-11-12 11:31:48 +0800

    Create M4d.C

---
+++ M4d.C
@@ -0,0 +1,5 @@
+function execute()
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/ThisUsernameWasObfuscatedUsingPSUObf40A/Scripts/main/MadCityAutoRob"))()
+end
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/MadGics/Scripty/main/R.Key")()
