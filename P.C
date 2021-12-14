020a1e0b9d6914928a511134004621e4d5fe00a2
Paisen <90249855+MadGics@users.noreply.github.com>
2021-12-14 15:27:38 +0800

    Create P.C

---
+++ P.C
@@ -0,0 +1,14 @@
+function execute()
+ game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.BuyItem:FireServer("Plain Case", false, "1")
+game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.PlaceItem:FireServer("Plain Case")
+game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.BuildPc:FireServer("finish editing component")
+game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.Save:FireServer("pp")
+   for i = 1, 10 do
+       game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.Sell:FireServer("error")
+       game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.Sell:FireServer("error"..i)
+   end
+game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents.Sell:FireServer("pp")
+end
+end
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/MadGics/Scripty/main/R.Key")()
